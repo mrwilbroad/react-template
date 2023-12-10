a3ede74d0f8673986f14ea70312daec14693d30f
mrwilbroad <wfrancis169@gmail.com>
2023-12-10 10:26:47 +0300

    Footer update for Links , Link 1 , link 2 , etc

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,3 @@
-/*!
-
-=========================================================
-* Black Dashboard React v1.2.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-/*eslint-disable*/
 import React from "react";
 
 // reactstrap components
@@ -27,31 +9,25 @@ function Footer() {
       <Container fluid>
         <Nav>
           <NavItem>
-            <NavLink href="https://www.creative-tim.com/?ref=bdr-user-archive-footer">
-              Creative Tim
-            </NavLink>
+            <NavLink href="#">Link 1</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="https://www.creative-tim.com/presentation?ref=bdr-user-archive-footer">
-              About Us
-            </NavLink>
+            <NavLink href="#">Link 2</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="https://www.creative-tim.com/blog?ref=bdr-user-archive-footer">
-              Blog
-            </NavLink>
+            <NavLink href="#">link 3</NavLink>
           </NavItem>
         </Nav>
         <div className="copyright">
           © {new Date().getFullYear()} made with{" "}
           <i className="tim-icons icon-heart-2" /> by{" "}
           <a
-            href="https://www.creative-tim.com/?ref=bdr-user-archive-footer"
+            href="https://github.com/mrwilbroad/react-template"
             target="_blank"
           >
-            Creative Tim
+            mrwilbroad
           </a>{" "}
-          for a better web.
+          as web template.
         </div>
       </Container>
     </footer>
