28ba81879f5f9bc05384fb4a4a22dddd1a2485f2
mrwilbroad <wfrancis169@gmail.com>
2023-12-10 09:46:28 +0300

    change rtl for language , routes removed

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,4 @@
-/*!
 
-=========================================================
-* Black Dashboard React v1.2.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import Dashboard from "views/Dashboard.js";
 import Icons from "views/Icons.js";
 import Map from "views/Map.js";
@@ -28,7 +12,6 @@ var routes = [
   {
     path: "/dashboard",
     name: "Dashboard",
-    rtlName: "لوحة القيادة",
     icon: "tim-icons icon-chart-pie-36",
     component: <Dashboard />,
     layout: "/admin",
@@ -36,7 +19,6 @@ var routes = [
   {
     path: "/icons",
     name: "Icons",
-    rtlName: "الرموز",
     icon: "tim-icons icon-atom",
     component: <Icons />,
     layout: "/admin",
@@ -52,7 +34,6 @@ var routes = [
   {
     path: "/notifications",
     name: "Notifications",
-    rtlName: "إخطارات",
     icon: "tim-icons icon-bell-55",
     component: <Notifications />,
     layout: "/admin",
@@ -60,7 +41,6 @@ var routes = [
   {
     path: "/user-profile",
     name: "User Profile",
-    rtlName: "ملف تعريفي للمستخدم",
     icon: "tim-icons icon-single-02",
     component: <UserProfile />,
     layout: "/admin",
@@ -68,7 +48,6 @@ var routes = [
   {
     path: "/tables",
     name: "Table List",
-    rtlName: "قائمة الجدول",
     icon: "tim-icons icon-puzzle-10",
     component: <TableList />,
     layout: "/admin",
@@ -76,18 +55,9 @@ var routes = [
   {
     path: "/typography",
     name: "Typography",
-    rtlName: "طباعة",
     icon: "tim-icons icon-align-center",
     component: <Typography />,
     layout: "/admin",
-  },
-  {
-    path: "/rtl-support",
-    name: "RTL Support",
-    rtlName: "ار تي ال",
-    icon: "tim-icons icon-world",
-    component: <Rtl />,
-    layout: "/rtl",
   },
 ];
 export default routes;
